e9da9fcbda2542aaa1a76bbd01e860fd2cab6164
nscando <nicolas.scandizzo@gmail.com>
2022-06-22 18:48:56 -0300

    mapping and class relations

---
+++ src/main/java/com/nscando/market/persistence/entity/Compra.java
@@ -0,0 +1,85 @@
+package com.nscando.market.persistence.entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Table(name = "compras")
+@Entity
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    private String estado;
+
+    @OneToMany(mappedBy = "producto")
+    private List<ComprasProducto> productos;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
+    //esto es para recuperar los datos de la entidad cliente, no para crear un cliente nuevo por eso el insertable y updatable false
+    private Cliente cliente;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/nscando/market/persistence/entity/Producto.java
@@ -0,0 +1,92 @@
+package com.nscando.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "productos")
+public class Producto {
+
+    @Id
+    //auto generacion del id en la BD
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //se usa para cuando tenes un nombre diferente a la de la BD en la clase
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name="precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_sotck")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+    @ManyToOne
+    //esto significa que a traves de esta relacion no vamos a borrar ni vamos a actualizar una nueva categoria
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
