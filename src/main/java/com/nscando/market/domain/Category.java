87ddee7b0173d3f9eef8ebd0522ac9697cb56dcc
nscando <nicolas.scandizzo@gmail.com>
2022-06-23 19:31:48 -0300

    Domain API targeting with MapStruct

---
+++ src/main/java/com/nscando/market/domain/Category.java
@@ -0,0 +1,35 @@
+package com.nscando.market.domain;
+
+public class Category {
+
+
+
+    private  int categoryId;
+    private String category;
+    private boolean active;
+
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
